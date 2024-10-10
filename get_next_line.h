b3893b3c2a2dd1eaaec9810c9beb477b6527f6fa
Yousef Kitaneh <yokitane@c2r5s1.42amman.com>
2024-10-10 18:11:15 +0300

    slow but passes the moulinette

---
+++ get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yokitane <yokitane@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/28 12:51:16 by yokitane          #+#    #+#             */
+/*   Updated: 2024/10/09 11:22:57 by yokitane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <fcntl.h>
+# include <unistd.h>
+# include <stddef.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_substr(char *s, int start, int len);
+int		ft_strlen(char *s);
+char	*ft_strdup(char *s);
+char	*ft_cutstash(char *stash, int nlindex);
+#endif
